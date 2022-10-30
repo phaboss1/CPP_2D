bc208f9134525628992c7717d3ff777078363479
yavas <extern.baris-doga.yavas@tttech-auto.com>
2022-10-30 22:43:47 +0300

    fix -d

--- cpp-2d/Main.cpp
+++ cpp-2d/Main.cpp
@@ -1,13 +1,22 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
+
+using namespace std;
 
 int main()
 {
 	b2World* world = new b2World(b2Vec2(0,0));
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Texture texture;
+	if (!texture.loadFromFile("C:/Users/baris.yavas/Desktop/CPP_2D/Debug/image.png"))
+	{
+		cout << "error" << endl;
+	}
 
 	while (window.isOpen())
 	{
