356379e1b67a9e63eb511c8a4770ed4f62fcf1fa
yavas <extern.baris-doga.yavas@tttech-auto.com>
2022-10-30 22:31:28 +0300

    init

---
+++ cpp-2d/Main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <box2d/box2d.h>
+
+int main()
+{
+	b2World* world = new b2World(b2Vec2(0,0));
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
